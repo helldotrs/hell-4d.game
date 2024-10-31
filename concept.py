06aa696bfb3dd060c1d18719668cf3c2fff06df6
Rebekah <macrosystem@proton.me>
2024-11-01 00:21:42 +0100

    Update concept.py

--- concept.py
+++ concept.py
@@ -8,12 +8,15 @@ player_pos = {
 
 class Pos:
     def __init__(self, x=0, y=0, z=0, q=0, t=0, item=None, creature=None, message=None, name=None):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.q = q
-        self.t = t
-        self.item = item
-        self.creature = creature
-        self.message = message
-        self.name = name
+        #pos
+        self.x    = x
+        self.y    = y
+        self.z    = z
+        self.q    = q
+        self.t    = t
+
+        #about
+        self.item        = item
+        self.creature    = creature
+        self.message     = message
+        self.name        = name
